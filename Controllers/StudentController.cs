29bef410059d8a074e10ddab7988df7260798868
dochitavasile1987 <dochitavasile1987@gmail.com>
2020-01-21 12:14:49 +0200

    1

--- Controllers/StudentController.cs
+++ Controllers/StudentController.cs
@@ -6,9 +6,36 @@ using System.Web.Mvc;
 
 namespace MyMVCApplication.Controllers
 {
+    //public class StudentController : Controller
+    //{
+    //    // GET: Student
+    //    public string Index() => "This is Index action method of StudentController";
+    //}
+    //public class StudentController : Controller
+    //{
+    //    public StudentController()
+    //    {
+
+    //    }
+
+    //    [NonAction]
+    //    public Student GetStudent(int id)
+    //    {
+    //        return studentList.Where(s => s.StudentId == id).FirstOrDefault();
+    //    }
+    //}
     public class StudentController : Controller
     {
-        // GET: Student
-        public string Index() => "This is Index action method of StudentController";
+        public StudentController()
+        {
+
+        }
+
+        [ActionName("find")]
+        public ActionResult GetById(int id)
+        {
+            // get student from the database 
+            return View();
+        }
     }
 }
