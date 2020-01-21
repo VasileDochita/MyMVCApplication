f8ba34e81b601925482f970d930c465d55b5ea44
dochitavasile1987 <dochitavasile1987@gmail.com>
2020-01-21 12:11:39 +0200

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyMVCApplication.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult EmptyResult()
+        {
+            ViewBag.Message = "This is an empty result";
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Edit(Student std)
+        {
+            // update student to the database
+
+            return RedirectToAction("Index");
+        }
+        [HttpDelete]
+        public ActionResult Delete(int Id)
+        {
+            // delete student from the database whose id matches with specified id
+
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Controllers/StudentController.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyMVCApplication.Controllers
+{
+    public class StudentController : Controller
+    {
+        // GET: Student
+        public string Index() => "This is Index action method of StudentController";
+    }
+}
